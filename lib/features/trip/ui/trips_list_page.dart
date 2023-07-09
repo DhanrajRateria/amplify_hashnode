ea2c29b79b83e8d2295ebe2796e67a0b5749d2f7
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-07-10 00:17:46 +0530

    Authentication

---
+++ lib/features/trip/ui/trips_list_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_hashnode/common/utils/colors.dart';
+
+class TripsListPage extends StatelessWidget {
+  const TripsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
