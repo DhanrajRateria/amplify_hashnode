c197e75779de8da0b5e37c61a7e4c836fb68d799
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-07-25 19:51:46 +0530

    Basic framework

--- lib/trips_planner_app.dart
+++ lib/trips_planner_app.dart
@@ -1,5 +1,7 @@
+import 'package:amplify_hashnode/features/trip/ui/edit_trip_page/edit_trip_page.dart';
 import 'package:amplify_hashnode/features/trip/ui/trip_page/trip_page.dart';
 import 'package:amplify_hashnode/features/trip/ui/trips_list_page.dart';
+import 'package:amplify_hashnode/models/ModelProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
@@ -19,11 +21,34 @@ class TripsPlannerApp extends StatelessWidget {
     final router = GoRouter(
       routes: [
         GoRoute(
+          path: '/',
+          name: AppRoute.home.name,
+          builder: (context, state) => isAmplifySuccessfullyConfigured
+              ? const TripsListPage()
+              : const Scaffold(
+                  body: Center(
+                    child: Text(
+                      'Tried to reconfigure Amplify; '
+                      'this can occur when your app restarts on Android.',
+                    ),
+                  ),
+                ),
+        ),
+        GoRoute(
           path: '/trip/:id',
           name: AppRoute.trip.name,
           builder: (context, state) {
             final tripId = state.params['id']!;
             return TripPage(tripId: tripId);
+          },
+        ),
+                GoRoute(
+          path: '/edittrip/:id',
+          name: AppRoute.edittrip.name,
+          builder: (context, state) {
+            return EditTripPage(
+              trip: state.extra! as Trip,
+            );
           },
         ),
       ],
